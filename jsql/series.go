54f7d11ec124d9e08f64641996aa8e31939737ed
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-09-24 08:31:01 -0500

    Backup:

--- jsql/series.go
+++ jsql/series.go
@@ -40,14 +40,15 @@ func DefineSeries(db *DB, schema string) (*Series, error) {
 		return nil, err
 	}
 
-	now := timezone.Now()
 	model.BeforeInsert(func(tx *Tx, old, new et.Json) error {
+		now := timezone.Now()
 		new.Set(CREATED_AT, now)
 		new.Set(UPDATED_AT, now)
 
 		return nil
 	}).
 		BeforeUpdate(func(tx *Tx, old, new et.Json) error {
+			now := timezone.Now()
 			new.Set(UPDATED_AT, now)
 
 			return nil
